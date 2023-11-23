5c710d77f26317741c4f23671cae7dfdadaf871d
unknown <pascalpahlevi@gmail.com>
2023-11-23 11:15:50 +0700

    Completed Tutorial 9

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:pbp_django_auth/pbp_django_auth.dart';
-import 'package:provider/provider.dart';
-import 'package:shopping_list/screens/login.dart';
 import 'package:shopping_list/widgets/left_drawer.dart';
-import 'package:shopping_list/screens/shoplist_form.dart';
-import 'package:shopping_list/screens/list_product.dart';
+import 'package:shopping_list/Widgets/shop_card.dart';
 
 class MyHomePage extends StatelessWidget {
   MyHomePage({Key? key}) : super(key: key);
@@ -70,86 +66,6 @@ class MyHomePage extends StatelessWidget {
                 }).toList(),
               ),
             ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class ShopItem {
-  final String name;
-  final IconData icon;
-
-  ShopItem(this.name, this.icon);
-}
-
-class ShopCard extends StatelessWidget {
-  final ShopItem item;
-
-  const ShopCard(this.item, {Key? key}); // Constructor
-
-  @override
-  Widget build(BuildContext context) {
-    final request = context.watch<CookieRequest>();
-    return Material(
-      color: Colors.indigo,
-      child: InkWell(
-        // Responsive touch area
-        onTap: () async {
-          // Show a SnackBar when clicked
-          ScaffoldMessenger.of(context)
-            ..hideCurrentSnackBar()
-            ..showSnackBar(SnackBar(
-                content: Text("You pressed the ${item.name} button!")));
-
-          if (item.name == "Add Product") {
-            Navigator.push(context,
-                MaterialPageRoute(builder: (context) => const ShopFormPage()));
-          } else if (item.name == 'View Products') {
-            Navigator.push(context,
-                MaterialPageRoute(builder: (context) => const ProductPage()));
-          } else if (item.name == "Logout") {
-            final response = await request.logout(
-                // TODO: Change the URL to your Django app's URL. Don't forget to add the trailing slash (/) if needed.
-                "http://127.0.0.1:8000/auth/logout/");
-            String message = response["message"];
-            if (response['status']) {
-              String uname = response["username"];
-              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                content: Text("$message Good bye, $uname."),
-              ));
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(builder: (context) => const LoginPage()),
-              );
-            } else {
-              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                content: Text("$message"),
-              ));
-            }
-          }
-        },
-        child: Container(
-          // Container to hold Icon and Text
-          padding: const EdgeInsets.all(8),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(
-                  item.icon,
-                  color: Colors.white,
-                  size: 30.0,
-                ),
-                const Padding(padding: EdgeInsets.all(3)),
-                Text(
-                  item.name,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(color: Colors.white),
-                ),
-              ],
-            ),
           ),
         ),
       ),
