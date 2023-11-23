4200c18c1b0423c050ecea3d0583bbc9fd2f6da1
unknown <pascalpahlevi@gmail.com>
2023-11-23 11:26:38 +0700

    Completed Tutorial 9

--- lib/Widgets/shop_card.dart
+++ lib/Widgets/shop_card.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:pbp_django_auth/pbp_django_auth.dart';
-import 'package:provider/provider.dart';
-import 'package:shopping_list/screens/login.dart';
 import 'package:shopping_list/screens/shoplist_form.dart';
+import 'package:shopping_list/screens/list_product.dart';
+import 'package:provider/provider.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:shopping_list/screens/login.dart';
 
 class ShopItem {
   final String name;
@@ -24,19 +25,26 @@ class ShopCard extends StatelessWidget {
       child: InkWell(
         // Responsive touch area
         onTap: () async {
-          // Show a SnackBar when clicked
+          // Show SnackBar when clicked
           ScaffoldMessenger.of(context)
             ..hideCurrentSnackBar()
             ..showSnackBar(SnackBar(
                 content: Text("You pressed the ${item.name} button!")));
 
+          // Navigate to the appropriate route (depending on the button type)
           if (item.name == "Add Product") {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const ShopFormPage(),
+              ),
+            );
+          } else if (item.name == "View Products") {
             Navigator.push(context,
-                MaterialPageRoute(builder: (context) => const ShopFormPage()));
+                MaterialPageRoute(builder: (context) => const ProductPage()));
           } else if (item.name == "Logout") {
-            final response = await request.logout(
-                // TODO: Change the URL to your Django app's URL. Don't forget to add the trailing slash (/) if needed.
-                "http://127.0.0.1:8000/auth/logout/");
+            final response =
+                await request.logout("http://127.0.0.1:8000/auth/logout/");
             String message = response["message"];
             if (response['status']) {
               String uname = response["username"];
